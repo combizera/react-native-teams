3fb8f92f0673a48c91a53f82da3162874c508460
combizera <ycombi349@gmail.com>
2025-03-12 14:52:06 -0300

    feat: status bar

--- app/screens/Groups/styles.ts
+++ app/screens/Groups/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 
 export const Container = styled.View
 `
-  background-color: ${({ theme }) => theme.COLORS.RED};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
   flex: 1;
   align-items: center;
   justify-content: center;
@@ -14,4 +14,5 @@ export const Title = styled.Text
 `
   font-size: 32px;
   font-weight: 700;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
 `;
--- components/Loading/styles.ts
+++ components/Loading/styles.ts
@@ -5,8 +5,6 @@ export const Container = styled.View
   flex: 1;
   justify-content: center;
   align-items: center;
-    
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `;
 
 export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme}) => ({
