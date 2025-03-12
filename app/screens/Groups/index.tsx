7fb5513daf4074e7b463a263676455d0ed4cda35
combizera <ycombi349@gmail.com>
2025-03-12 18:00:28 -0300

    feat: component GroupCard + give a component TouchableOpacityProps

--- app/screens/Groups/index.tsx
+++ app/screens/Groups/index.tsx
@@ -1,14 +1,20 @@
 import * as S from './styles';
 import { Header } from '@components/Header';
 import { Highligth } from '@components/Highligth';
+import { GroupCard } from '@components/GroupCard';
 
 export default function Groups() {
   return (
     <S.Container>
       <Header showBackButton />
+
       <Highligth
         title="Turmas"
         subtitle='Jogue com a sua turma'
+      />
+
+      <GroupCard
+        title="Artes Cênicas"
       />
     </S.Container>
   );
