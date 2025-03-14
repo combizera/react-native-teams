70a63cd2255f416b803faa3fae1e438324f5ed72
combizera <ycombi349@gmail.com>
2025-03-14 10:20:03 -0300

    feat: component Input

--- app/screens/NewGroup/index.tsx
+++ app/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Highligth } from '@components/Highligth';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -16,8 +17,11 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar as pessoas"
         />
 
+        <Input />
+
         <Button
           title="Criar"
+          style={{ marginTop: 20 }}
         />
 
       </Content>
