3b7c99f51037059b7c87b90ebff9ce691142411a
combizera <ycombi349@gmail.com>
2025-03-16 19:01:27 -0300

    feat: component PlayerCard

--- app/screens/Players/index.tsx
+++ app/screens/Players/index.tsx
@@ -7,10 +7,11 @@ import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
+import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Ygor', 'Combi']);
 
   return (
     <Container>
@@ -50,6 +51,18 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => { }}
+          />
+        )}
+      />
+
     </Container >
   );
 }
