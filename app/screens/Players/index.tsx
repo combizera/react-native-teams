6421f2ef10008f25bc682eb60346bc6eeff11c22
combizera <ycombi349@gmail.com>
2025-03-16 18:22:57 -0300

    wip

--- app/screens/Players/index.tsx
+++ app/screens/Players/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@components/Header";
 import { Highligth } from "@components/Highligth";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
 export function Players() {
   return (
@@ -25,6 +26,10 @@ export function Players() {
           icon="add"
         />
       </Form>
+
+      <Filter
+        title="Time A"
+      />
     </Container>
   );
 }
