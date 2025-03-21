c909111e79eb0ba03f98d11356b5525da140c461
combizera <ycombi349@gmail.com>
2025-03-21 09:47:23 -0300

    feat: using SafeAreaView for better UX

--- app/screens/NewGroup/styles.ts
+++ app/screens/NewGroup/styles.ts
@@ -1,16 +1,15 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { UsersThree } from "phosphor-react-native";
 
 import styled from 'styled-components/native';
 
-export const Container = styled.View
-`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
 `
 
-export const Content = styled.View
-`
+export const Content = styled.View`
   flex: 1;
   justify-content: center;
 `
